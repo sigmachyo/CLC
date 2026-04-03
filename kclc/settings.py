c76f17d07058e1f40e70bac92072c3cea5de3029
sigmachyo <sandstormbim@gmail.com>
2026-04-03 15:41:37 +0700

    Admin UI: Integrated django-jazzmin with Midnight Gold Theme

--- kclc/settings.py
+++ kclc/settings.py
@@ -30,6 +30,7 @@ VAPID_PRIVATE_KEY = 'a4IVr9jA_SyFG-cohYOfztaM0Ul3xrXZI8qgl6a_KKk'
 VAPID_ADMIN_EMAIL = 'mailto:webmaster@kclc.ru'
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,84 @@ LOGOUT_REDIRECT_URL = '/'  # Куда перенаправлять после в
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SESSION_COOKIE_HTTPONLY = True
+
+# -------------------------------------------------------------
+# JAZZMIN ADMIN CONFIGURATION (Midnight Gold Theme)
+# -------------------------------------------------------------
+JAZZMIN_SETTINGS = {
+    "site_title": "CLC Panel",
+    "site_header": "CLC Admin",
+    "site_brand": "CLC Красноярск",
+    "site_icon": "fas fa-church",
+    "site_logo_classes": "img-circle",
+    "welcome_sign": "Добро пожаловать в панель CLC",
+    "copyright": "CLC Krasnoyarsk",
+    "search_model": ["auth.User", "church_app.Event"],
+    "user_avatar": None,
+    "topmenu_links": [
+        {"name": "На сайт", "url": "/", "new_window": True},
+    ],
+    "show_sidebar": True,
+    "navigation_expanded": True,
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "church_app.Video": "fas fa-video",
+        "church_app.Announcement": "fas fa-bullhorn",
+        "church_app.SpiritualLevel": "fas fa-route",
+        "church_app.DailyVerse": "fas fa-book-open",
+        "church_app.Event": "fas fa-calendar-check",
+        "church_app.EventRegistration": "fas fa-ticket-alt",
+        "church_app.PrayerRequest": "fas fa-pray",
+        "church_app.KidsContent": "fas fa-child",
+        "church_app.KidsProgress": "fas fa-star",
+        "church_app.ChatRoom": "fas fa-comments",
+        "church_app.ChatMessage": "fas fa-comment-dots",
+        "church_app.Category": "fas fa-tags",
+        "church_app.BiblePlan": "fas fa-book",
+        "church_app.BibleReading": "fas fa-bookmark",
+        "church_app.UserProgress": "fas fa-chart-line",
+        "church_app.UserBibleProgress": "fas fa-tasks",
+        "church_app.PushSubscription": "fas fa-bell",
+    },
+    "default_icon_parents": "fas fa-chevron-circle-right",
+    "default_icon_children": "fas fa-circle",
+    "related_modal_active": False,
+    "custom_css": None,
+    "custom_js": None,
+    "show_ui_builder": False,
+    "order_with_respect_to": ["church_app", "church_app.DailyVerse", "church_app.Event", "church_app.Announcement", "church_app.Video", "church_app.PrayerRequest"],
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-navy",
+    "accent": "accent-warning",
+    "navbar": "navbar-white navbar-light",
+    "no_navbar_border": False,
+    "navbar_fixed": True,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": True,
+    "sidebar": "sidebar-dark-navy",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": True,
+    "theme": "default",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-outline-primary",
+        "secondary": "btn-outline-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
